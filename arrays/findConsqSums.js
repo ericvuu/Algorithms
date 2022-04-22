84f449e5848b0becdc749ec022737053bf51993d
Eric Vuu <66089946+ericvuu@users.noreply.github.com>
2022-04-22 11:56:11 -0700

    Add files via upload

---
+++ arrays/findConsqSums.js
@@ -0,0 +1,47 @@
+// Interview Algo given to alumni
+
+/* 
+  You are given a list of integers. Find all the consecutive sets of 
+  integers that adds up to the sum passed in as one of the inputs.
+*/
+
+const nums1 = [2, 5, 3, 6, 7, 23, 12];
+const sum1 = 16;
+const expected1 = [
+  [2, 5, 3, 6],
+  [3, 6, 7],
+];
+
+// Bonus:
+const nums2 = [2, 5, 3, 6, 7, 0, 0, 23, 12];
+const sum2 = 16;
+const expected2 = [
+  [2, 5, 3, 6],
+  [3, 6, 7],
+  [3, 6, 7, 0],
+  [3, 6, 7, 0, 0],
+];
+
+// Bonus:
+const nums3 = [-2, -5, -3, -6, -7, -0, -0, -23, -12];
+const sum3 = -16;
+const expected2 = [
+  [-2, -5, -3, -6],
+  [-3, -6, -7],
+  [-3, -6, -7, -0],
+  [-3, -6, -7, -0, -0],
+];
+
+function findConsqSums(nums, targetSum) {
+  let arr = [];
+  for (let i = 0; i < nums.length; i++) {
+    let sum = nums[i];
+    for (let j = i + 1; j <= nums.length; j++) {
+      if (sum == targetSum) {
+        arr.push(nums.slice(i, j));
+      }
+      sum += nums[j];
+    }
+  }
+  return arr;
+}
---
+++ arrays/firstNonRepeated.js
@@ -0,0 +1,44 @@
+/* 
+  Given an array of integers
+  return the first integer from the array that is not repeated anywhere else
+  If there are multiple non-repeated integers in the array,
+  the "first" one will be the one with the lowest index.
+*/
+
+const nums1 = [3, 5, 4, 3, 4, 6, 5];
+const expected1 = 6;
+
+const nums2 = [3, 5, 5];
+const expected2 = 3;
+
+const nums3 = [3, 3, 5];
+const expected3 = 5;
+
+const nums4 = [5];
+const expected4 = 5;
+
+const nums5 = [];
+const expected5 = null;
+
+/**
+ * Finds the first int from the given array that has no duplicates. I.e., the
+ *    item at the lowest index that doesn't appear again in the given array.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<number>} nums
+ * @return {number|null} The first int value from the given array that has no
+ *    dupes or null if there is none.
+ */
+function firstNonRepeated(nums) {
+  for (let i = 0; i < nums.length; i++) {
+    let num = nums[i];
+    if (nums.indexOf(num) == i && nums.indexOf(num, i + 1) == -1) {
+      return num;
+    }
+  }
+  return null;
+}
+
+console.log(firstNonRepeated(nums5))
+
+module.exports = { firstNonRepeated };
